b2e3f72b66a13ba117073e63302afe2acbc3c481
Eatableninjago11 <83182611+Eatableninjago11@users.noreply.github.com>
2022-02-03 11:09:01 -0600

    Add files via upload

---
+++ v.py
@@ -0,0 +1,61 @@
+from cv2 import mean
+import plotly.express as px
+import plotly.figure_factory as ff
+import plotly.graph_objects as go
+import pandas as pd
+import csv
+import statistics
+import random
+
+
+df = pd.read_csv("medium_data.csv")
+
+data = df["publication"].tolist()
+def random_set_of_mean(counter):
+    dataset = []
+    for i in range(0, counter):
+        random_index= random.randint(0,len(data)-1)
+        value = data[random_index]
+        dataset.append(value)
+    mean = statistics.mean(dataset)
+    return mean
+
+#function to plot the mean on the graph
+def show_fig(mean_list):
+    df = mean_list
+    mean = statistics.mean(df)
+    fig = ff.create_distplot([df], ["publication"], show_hist=False)
+    fig.add_trace(go.Scatter(x=[mean, mean], y=[0, 1], mode="lines", name="MEAN"))
+    fig.show()
+
+
+# Pass the number of time you want the mean of the data points as a parameter in range function in for loop
+def setup():
+    mean_list = []
+    for i in range(0,1000):
+        set_of_means= random_set_of_mean(100)
+        mean_list.append(set_of_means)
+    show_fig(mean_list)
+    
+    mean = statistics.mean(mean_list)
+    print("Mean of sampling distribution :-",mean )
+
+setup()
+
+
+#Code to find the mean of the raw data ("population data")
+publication_mean = statistics.mean(data)
+print("publication mean:- ", publication_mean)
+
+
+# code to find the standard deviation of the sample data
+def standard_deviation():
+    mean_list = []
+    for i in range(0,1000):
+        set_of_means= random_set_of_mean(100)
+        mean_list.append(set_of_means)
+
+    std_deviation = statistics.stdev(mean_list)
+    print("Standard deviation of sampling distribution:- ", std_deviation)
+
+standard_deviation()
